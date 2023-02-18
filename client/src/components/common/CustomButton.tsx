9199dab0af503b24726611f2579d05c6390a828f
Ali Abbasi <al5690410@gmail.com>
2023-02-19 04:22:44 +0500

    added: create property, form, all properties

--- client/src/components/common/CustomButton.tsx
+++ client/src/components/common/CustomButton.tsx
@@ -1,7 +1,38 @@
-import React from "react";
+import { Button } from "@pankod/refine-mui";
+import { CustomButtonProps } from "interfaces/common";
 
-const CustomButton = () => {
-  return <div></div>;
+const CustomButton = ({
+  type,
+  title,
+  backgroundColor,
+  color,
+  fullWidth,
+  icon,
+  handleClick,
+}: CustomButtonProps) => {
+  return (
+    <Button
+      sx={{
+        flex: fullWidth ? 1 : "unset",
+        padding: "10px 15px",
+        width: fullWidth ? "100%" : "fit-content",
+        minWidth: 130,
+        backgroundColor,
+        color,
+        fontSize: 16,
+        fontWeight: 600,
+        textTransform: "capitalize",
+        gap: "10px",
+        "&:hover": {
+          opacity: 0.9,
+          backgroundColor,
+        },
+      }}
+      onClick={handleClick}
+    >
+      {icon} {title}
+    </Button>
+  );
 };
 
 export default CustomButton;
