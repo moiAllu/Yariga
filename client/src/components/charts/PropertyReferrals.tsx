7ccb966562ff5357f4d2abf81efeece156430ef7
Ali Abbasi <al5690410@gmail.com>
2023-02-18 22:46:44 +0500

    added: Apex charts, custom buttons, property cards

--- client/src/components/charts/PropertyReferrals.tsx
+++ client/src/components/charts/PropertyReferrals.tsx
@@ -1,7 +1,60 @@
-import React from "react";
+import { Box, Stack, Typography } from "@pankod/refine-mui";
+import { propertyReferralsInfo } from "../../constants";
 
+interface ProgressBarProps {
+  title: string;
+  percentage: number;
+  color: string;
+}
+const ProgressBar = ({ title, percentage, color }: ProgressBarProps) => (
+  <Box width="100%">
+    <Stack direction="row" alignContent="center" justifyContent="space-between">
+      <Typography fontSize={16} fontWeight={500} color="#11142d">
+        {title}
+      </Typography>
+      <Typography fontSize={16} fontWeight={500} color="#11142d">
+        {percentage}%
+      </Typography>
+    </Stack>
+    <Box
+      mt={2}
+      width="100%"
+      borderRadius={1}
+      height="8px"
+      bgcolor="#e4e8ef"
+      position="relative"
+    >
+      <Box
+        width={`${percentage}%`}
+        bgcolor={color}
+        borderRadius={1}
+        height="100%"
+      />
+    </Box>
+  </Box>
+);
 const PropertyReferrals = () => {
-  return <div></div>;
+  return (
+    <Box
+      p={4}
+      bgcolor="#fcfcfc"
+      id="chart"
+      display="flex"
+      flexDirection="column"
+      borderRadius="15px"
+      minWidth={490}
+    >
+      <Typography fontSize={18} fontWeight={600} color="#11142d">
+        Property Referrals
+      </Typography>
+      <Stack my="20px" gap={4} direction="column">
+        {/* {propertyReferralsInfo} */}
+        {propertyReferralsInfo.map((bar) => (
+          <ProgressBar key={bar.title} {...bar} />
+        ))}
+      </Stack>
+    </Box>
+  );
 };
 
 export default PropertyReferrals;
--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,6 +1,56 @@
 import { useList } from "@pankod/refine-core";
+import {
+  PieChart,
+  PropertyReferrals,
+  TotalRevenue,
+  TopAgent,
+  PropertyCard,
+} from "../components";
+import { Typography, Box, Stack } from "@pankod/refine-mui";
 const home = () => {
-  return <div></div>;
+  return (
+    <Box>
+      <Typography fontSize={25} fontWeight={700} color="#11142d">
+        Dashboard
+      </Typography>
+      <Box mt="20px" display="flex" flexWrap="wrap" gap={4}>
+        {" "}
+        <PieChart
+          title="Property for Sale"
+          value={645}
+          series={[75, 25]}
+          colors={["#475be8", "#e4e8ef"]}
+        />{" "}
+        <PieChart
+          title="Property for rent"
+          value={645}
+          series={[60, 45]}
+          colors={["#475be8", "#e4e8ef"]}
+        />{" "}
+        <PieChart
+          title="Property for customers"
+          value={5645}
+          series={[75, 25]}
+          colors={["#475ba8", "#e4e8cf"]}
+        />{" "}
+        <PieChart
+          title="Property for cities"
+          value={645}
+          series={[85, 35]}
+          colors={["#475be8", "#e4e8ef"]}
+        />
+      </Box>
+      <Stack
+        mt="25px"
+        width="100%"
+        direction={{ xs: "column", lg: "row" }}
+        gap={4}
+      >
+        <TotalRevenue />
+        <PropertyReferrals />
+      </Stack>
+    </Box>
+  );
 };
 
 export default home;
