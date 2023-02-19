8d538fdd33cd2aa738882b17b5448ae4f08e316e
Ali Abbasi <al5690410@gmail.com>
2023-02-19 05:10:51 +0500

    added:CRUD

---
+++ server/controllers/property.controller.js
@@ -0,0 +1,15 @@
+import Property from "../mongodb/models/property.js";
+import User from "../mongodb/models/user.js";
+const getAllProperties = async (req, res) => {};
+const getPropertyDetail = async (req, res) => {};
+const createProperty = async (req, res) => {};
+const editProperty = async (req, res) => {};
+const deleteProperty = async (req, res) => {};
+
+export {
+  getAllProperties,
+  getPropertyDetail,
+  createProperty,
+  editProperty,
+  deleteProperty,
+};
---
+++ server/routes/user.routes.js
@@ -0,0 +1,3 @@
+import express from "express";
+
+//import all controllers of users
