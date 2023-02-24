b9e6887fbd57d71382c2220da01edbb7ef5b88e4
Ali Abbasi <al5690410@gmail.com>
2023-02-25 02:37:51 +0500

    updated: home page

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -7,7 +7,21 @@ import {
   PropertyCard,
 } from "../components";
 import { Typography, Box, Stack } from "@pankod/refine-mui";
-const home = () => {
+const Home = () => {
+  const { data, isLoading, isError } = useList({
+    resource: "properties",
+    config: {
+      pagination: {
+        pageSize: 4,
+      },
+    },
+  });
+
+  const latestProperties = data?.data ?? [];
+
+  if (isLoading) return <Typography>Loading...</Typography>;
+  if (isError) return <Typography>Something went wrong!</Typography>;
+
   return (
     <Box>
       <Typography fontSize={25} fontWeight={700} color="#11142d">
@@ -49,8 +63,35 @@ const home = () => {
         <TotalRevenue />
         <PropertyReferrals />
       </Stack>
+      <Box
+        flex={1}
+        borderRadius="15px"
+        padding="20px"
+        bgcolor="#fcfcfc"
+        display="flex"
+        flexDirection="column"
+        minWidth="100%"
+        mt="25px"
+      >
+        <Typography fontSize="18px" fontWeight={600} color="#11142d">
+          Latest Properties
+        </Typography>
+
+        <Box mt={2.5} sx={{ display: "flex", flexWrap: "wrap", gap: 4 }}>
+          {latestProperties.map((property) => (
+            <PropertyCard
+              key={property._id}
+              id={property._id}
+              title={property.title}
+              location={property.location}
+              price={property.price}
+              photo={property.photo}
+            />
+          ))}
+        </Box>
+      </Box>
     </Box>
   );
 };
 
-export default home;
+export default Home;
